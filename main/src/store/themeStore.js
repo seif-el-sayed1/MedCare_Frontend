6cd5318e1a2361ae6912f309bbb7dad36887a08b
seif_elsayed <seifsayed121@gmail.com>
2026-06-03 01:34:04 +0300

    global store folder

---
+++ main/src/store/themeStore.js
@@ -0,0 +1,25 @@
+import { create } from 'zustand'
+
+const savedTheme = localStorage.getItem('theme') || 'light'
+if (savedTheme === 'dark') {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+const useThemeStore = create((set) => ({
+  theme: savedTheme,
+  toggleTheme: () =>
+    set((state) => {
+      const newTheme = state.theme === 'light' ? 'dark' : 'light'
+      localStorage.setItem('theme', newTheme)
+      if (newTheme === 'dark') {
+        document.documentElement.classList.add('dark')
+      } else {
+        document.documentElement.classList.remove('dark')
+      }
+      return { theme: newTheme }
+    }),
+}))
+
+export default useThemeStore
