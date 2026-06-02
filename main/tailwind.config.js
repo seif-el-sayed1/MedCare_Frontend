3c13ed24f764ebc4c83f60e540997c702bd1c4a6
seif_elsayed <seifsayed121@gmail.com>
2026-06-03 01:32:56 +0300

    app init

--- main/tailwind.config.js
+++ main/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -14,6 +15,24 @@ export default {
           600: '#16a34a',
           700: '#15803d',
           900: '#14532d',
+        },
+        neutralSurface: {
+          50:  '#f8fafc',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+        },
+        tertiary: {
+          50:  '#f8fafc',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+          400: '#94a3b8',
+          500: '#64748b',
+          600: '#475569',
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a',
         },
       },
       fontFamily: {
