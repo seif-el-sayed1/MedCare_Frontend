2f7c468730d4a134ed424a0ca09bb32457587b89
seif_elsayed <seifsayed121@gmail.com>
2026-06-03 01:29:25 +0300

    add app colors

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,13 +9,30 @@ export default {
     extend: {
       colors: {
         primary: {
+          50:  '#f0fdfa',
+          100: '#ccfbf1',
+          500: '#14B8A6', 
+          600: '#0d9488',
+          700: '#0f766e', 
+          950: '#022c22',
+        },
+        secondary: {
           50:  '#f0f9ff',
           100: '#e0f2fe',
-          500: '#0ea5e9',
+          400: '#38bdf8',
+          500: '#0EA5E9', 
           600: '#0284c7',
-          700: '#0369a1',
-          900: '#0c4a6e',
         },
+        tertiary: {
+          400: '#94a3b8',
+          500: '#64748B', 
+          600: '#475569',
+          900: '#0f172a', 
+        },
+        neutralSurface: {
+          50:  '#F8FAFC',
+          100: '#f1f5f9',
+        }
       },
       fontFamily: {
         sans:   ['Inter', 'sans-serif'],
